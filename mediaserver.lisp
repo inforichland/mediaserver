d8f4338fa7e7d8b597921a80d29ee9a4d723f129
Tim Wawrzynczak <inforichland@gmail.com>
2010-01-28 21:08:09 -0600

    added initial id3 parsing code

--- mediaserver.lisp
+++ mediaserver.lisp
@@ -12,18 +12,50 @@
 
 ;; the media server code
 
-(defparameter *songs* (make-array
-		       100
-		       :element-type 'pathname
-		       :adjustable t
-		       :fill-pointer 0))
+(defparameter *songs* nil)
 
 (defun search-media (directory)
-  (declare (ftype (function (string) nil) search-media))
-  (cl-fad:walk-directory
-   directory
-   #'(lambda (path)
-       (when (equal "mp3" (pathname-type path))
-	 (vector-push-extend path *songs*))))
-  (values))
+  (declare (ftype (function (string) (vector t)) search-media))
+  (let ((songs (make-array 100 :element-type 'pathname :adjustable t :fill-pointer 0)))
+    (cl-fad:walk-directory
+     directory
+     #'(lambda (path)
+	 (when (equal "mp3" (pathname-type path))
+	   (vector-push-extend path songs))))
+    songs))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ID3 parsing code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; all we really need out of the ID3
+;; tag is the title, artist, album, genre
+
+(defstruct song-info
+  title
+  artist
+  album
+  genre)
+
+(defun synchsafe (bytes)
+  (reduce #'(lambda (x a)
+	      (logior (ash x 7) a)) bytes :initial-value 0))
+
+(defun id3-version (filename)
+  (with-open-file (file filename)
+    (let* ((len (file-length file))
+	   (tagpos (- len 128))
+	   (tag (make-array 3)))
+      (if (<= tagpos 0)
+	  'unknown
+	  (progn
+	    (file-position file tagpos)
+	    (read-sequence tag file)
+	    (cond ((string= (coerce tag 'string) "TAG") 'id3v1)
+		  (t (progn
+		       (file-position file 0)
+		       (read-sequence tag file)
+		       (if (string= (coerce tag 'string) "ID3")
+			   'id3v2
+			   'unknown)))))))))
 
